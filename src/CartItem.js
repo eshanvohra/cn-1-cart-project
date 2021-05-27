1c4bed07cdf91601f681ed0fab6ea799fd4ba3ae
Eshan Vohra <eshanvohra@gmail.com>
2021-05-27 19:46:34 +0530

    Added State

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css';
 const CartItem=()=>{
+    const [cartItem,setItem]=useState({
+        price:100,
+        title:"Phone",
+        Qty:12,
+        img:'https://image.flaticon.com/icons/png/512/3020/3020657.png'
+    })
+    const {price,title,Qty,img}=cartItem;
     return (
         <>
         <div className='cart-item'>
             <div className='left-block'>
-                <img style={styles.image}/>
+                <img style={styles.image} src={img}/>
             </div>
             <div className='right-block'>
-            <div>Phone</div>
-            <div>Rs.10000</div>
-            <div>Qty:1</div>
+            <div>{title}</div>
+            <div>{price}</div>
+            <div>{Qty}</div>
                 <div className='cart-item-actions'>
-                    <div className='action-icons'>
-
-                    </div>
+                    <img alt="increase" src="https://image.flaticon.com/icons/png/512/992/992651.png" className='action-icons'/>
+                    <img alt="decrease" src="https://image.flaticon.com/icons/png/512/992/992683.png" className='action-icons'/>
+                    <img alt="delete" src="https://image.flaticon.com/icons/png/512/3096/3096673.png" className='action-icons'/>
                 </div>
             </div>
         </div>
